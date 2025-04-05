c2ef0acc84ee302b72bc18926aec251975979324
akachan <i@neri.moe>
2025-04-06 02:18:20 +0800

    实现了HIDDevice和一些对象的再包装

--- lib/neo_web_hid.dart
+++ lib/neo_web_hid.dart
@@ -1,14 +1,75 @@
+import 'dart:typed_data';
+
 import 'package:web/web.dart';
 import 'dart:js_interop';
 
 @JS('navigator.hid')
-external Hid? get hid;
+external JSHid? get hid;
 
 bool canUseHid() => hid != null;
 
-extension type Hid._(EventTarget _) implements EventTarget {
-  external JSPromise<JSArray<HIDDevice>> getDevices();
-  external JSPromise<JSArray<HIDDevice>> requestDevice(JSArray option);
+class Hid {}
+
+class HIDDevice {
+  final JSHIDDevice _interop;
+  HIDDevice(this._interop);
+
+  bool get opened => _interop.opened.toDart;
+  int get vendorId => _interop.vendorId.toDartInt;
+  int get productId => _interop.productId.toDartInt;
+  String get productName => _interop.productName.toDart;
+  List<HIDCollection> get collections =>
+      _interop.collections.toDart.map((e) => HIDCollection(e)).toList();
+
+  Future open() => _interop.open().toDart;
+  Future close() => _interop.close().toDart;
+  Future forget() => _interop.forget().toDart;
+  Future<ByteData> receiveFeatureReport(int reportId) => _interop
+      .receiveFeatureReport(reportId.toJS)
+      .toDart
+      .then((value) => value.toDart);
+  Future sendFeatureReport(int reportId, ByteData data) =>
+      _interop.sendFeatureReport(reportId.toJS, data.toJS).toDart;
+  Future sendReport(int reportId, ByteData data) =>
+      _interop.sendReport(reportId.toJS, data.toJS).toDart;
+
+  Function(HIDInputReportEvent event)? oninputreport;
+
+  void onInputReport(Function(HIDInputReportEvent event) func) {
+    _interop.oninputreport = _onInputReport.toJS;
+    oninputreport = func;
+  }
+
+  void _onInputReport(JSHIDInputReportEvent event) {
+    if (oninputreport != null) {
+      oninputreport!(HIDInputReportEvent(event));
+    }
+  }
+}
+
+class HIDCollection {
+  final JSHIDCollection _interop;
+  HIDCollection(this._interop);
+
+  int get usagePage => _interop.usagePage.toDartInt;
+  int get usage => _interop.usage.toDartInt;
+  int get type => _interop.type.toDartInt;
+  List<HIDCollection> get children =>
+      _interop.children.toDart.map((e) => HIDCollection(e)).toList();
+}
+
+class HIDInputReportEvent {
+  final JSHIDInputReportEvent _interop;
+  HIDInputReportEvent(this._interop);
+
+  ByteData get data => _interop.data.toDart;
+  HIDDevice get device => HIDDevice(_interop.device);
+  int get reportId => _interop.reportId.toDartInt;
+}
+
+extension type JSHid._(EventTarget _) implements EventTarget {
+  external JSPromise<JSArray<JSHIDDevice>> getDevices();
+  external JSPromise<JSArray<JSHIDDevice>> requestDevice(JSArray option);
 
   external EventHandler get onconnect;
   external EventHandler get ondisconnect;
@@ -16,39 +77,39 @@ extension type Hid._(EventTarget _) implements EventTarget {
   external set ondisconnect(EventHandler value);
 }
 
-extension type HIDDevice._(EventTarget _) implements EventTarget {
-  external bool get opened;
-  external int get vendorId;
-  external int get productId;
-  external String get productName;
-  external JSArray<Collection> get collections;
+extension type JSHIDDevice._(EventTarget _) implements EventTarget {
+  external JSBoolean get opened;
+  external JSNumber get vendorId;
+  external JSNumber get productId;
+  external JSString get productName;
+  external JSArray<JSHIDCollection> get collections;
 
   external JSPromise open();
   external JSPromise close();
   external JSPromise forget();
-  external JSPromise<JSDataView> receiveFeatureReport(int reportId);
-  external JSPromise sendFeatureReport(int reportId, JSDataView data);
-  external JSPromise sendReport(int reportId, JSDataView data);
+  external JSPromise<JSDataView> receiveFeatureReport(JSNumber reportId);
+  external JSPromise sendFeatureReport(JSNumber reportId, JSDataView data);
+  external JSPromise sendReport(JSNumber reportId, JSDataView data);
 
   external EventHandler get oninputreport;
   external set oninputreport(EventHandler value);
 }
 
-extension type HIDConnectionEvent._(Event _) implements Event {
-  external HIDDevice get device;
+extension type JSHIDConnectionEvent._(Event _) implements Event {
+  external JSHIDDevice get device;
 }
 
-extension type HIDInputReportEvent._(Event _) implements Event {
+extension type JSHIDInputReportEvent._(Event _) implements Event {
   external JSDataView get data;
-  external HIDDevice get device;
-  external int get reportId;
+  external JSHIDDevice get device;
+  external JSNumber get reportId;
 }
 
-extension type Collection._(JSObject _) implements JSObject {
-  external int get usagePage;
-  external int get usage;
-  external int get type;
-  external JSArray<Collection> get children;
+extension type JSHIDCollection._(JSObject _) implements JSObject {
+  external JSNumber get usagePage;
+  external JSNumber get usage;
+  external JSNumber get type;
+  external JSArray<JSHIDCollection> get children;
 }
 
 /*
